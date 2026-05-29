273d4374b89880cb3428c09d947c04daf58b6067
lcnhsk3 <lcnpalembang@gmail.com>
2026-05-29 12:24:50 +0700

    Add files via upload

---
+++ data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{F91FA01F-5ABD-46A7-9AC1-4AC3D101692B}'];loadHandler&&loadHandler(0, '<div id="spr0_cc5d6"><div id="spr1_cc5d6" class="kern slide"><img id="img0_cc5d6" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_cc5d6" class="kern slide"><div id="wo_0_0"></div></div></div>', '{"s":[]}');})();
